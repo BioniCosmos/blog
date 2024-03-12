BioniCosmos/blog

commit 2734a347e151602b6f48761d4bd393e05316cc15

Support Mermaid
Upgrade Astro to fix [an audit related issue](https://github.com/withastro/astro/pull/10163).

tailwind.config.ts
@@ -55,7 +55,7 @@ export default {
           '&::before': { content: '"["' },
           '&::after': { content: '"]"' },
         },
-        img: {
+        'img, svg': {
           marginLeft: 'auto',
           marginRight: 'auto',
         },

astro.config.ts
@@ -2,6 +2,7 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 import rehypeMathjax from 'rehype-mathjax'
 import remarkMath from 'remark-math'
+import remarkMermaid from 'remark-mermaidjs'
 import { abstract } from './src/utils/abstract'
 import {
   extractRefInImageCaption,
@@ -14,7 +15,12 @@ import { openExternalLinksInNewTab } from './src/utils/link'
 export default defineConfig({
   integrations: [tailwind()],
   markdown: {
-    remarkPlugins: [abstract, extractRefInImageCaption, remarkMath],
+    remarkPlugins: [
+      abstract,
+      extractRefInImageCaption,
+      remarkMath,
+      remarkMermaid,
+    ],
     rehypePlugins: [
       imageCaption,
       removeParagraph,

commit b5784a480cfd14a309f316ef311940b67a8d9f44

Fix a navbar display issue
The issue was introduced in commit 2734a347e151602b6f48761d4bd393e05316cc15.

tailwind.config.ts
@@ -55,7 +55,7 @@ export default {
           '&::before': { content: '"["' },
           '&::after': { content: '"]"' },
         },
-        'img, svg': {
+        img: {
           marginLeft: 'auto',
           marginRight: 'auto',
         },
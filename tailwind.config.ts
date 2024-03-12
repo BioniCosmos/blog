b5784a480cfd14a309f316ef311940b67a8d9f44
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2024-03-12 19:01:46 +0800

    Fix a navbar display issue
    The issue was introduced in commit 2734a347e151602b6f48761d4bd393e05316cc15.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,7 @@ export default {
           '&::before': { content: '"["' },
           '&::after': { content: '"]"' },
         },
-        'img, svg': {
+        img: {
           marginLeft: 'auto',
           marginRight: 'auto',
         },
